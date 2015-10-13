0b45a4a0ad4efb8b670decdf644b1022a5831f9d
WangMichael <WangMichael@users.noreply.github.com>
2015-10-13 14:01:28 +0100

    added doc comments to Job

--- Job.java
+++ Job.java
@@ -1,4 +1,10 @@
 
+/**
+ * Write a description of class Job here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
   public class Job implements Runnable 
 {
     private int jobNumber;
